930307e39ee00de56e4543bef9be9a758e304e15
Jozy Kinnaman <jozypants@gmail.com>
2020-03-26 14:13:27 -0700

    Add branching
    co authored by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var gender = $("#gender").val();
+    var quality = $("#quality").val();
+    var date = $("#date").val();
+
+    if (gender === "male") {
+      if (quality === "quality-1" && date === "date-1") {
+        $("#kevin").show();
+      }
+    }
+
+    event.preventDefault();
+  });
+});
