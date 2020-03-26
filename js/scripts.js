8652d79963dc9dd84146fe98a3b5dd46637ff83d
Jozy Kinnaman <jozypants@gmail.com>
2020-03-26 14:54:44 -0700

    add else if statements to javascript.
    coauthored by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,24 @@ $(document).ready(function() {
     var quality = $("#quality").val();
     var date = $("#date").val();
 
-    if (gender === "male") {
-      if (quality === "quality-1" && date === "date-1") {
-        $("#kevin").show();
+    if (date === "date-1") {
+      $("#meredith").show();
+    } else if (quality === "quality-2" && date === "date-2") {
+      $("#pam").show();
+      $("#meredith").hide();
+    } else if (quality === "quality-3" && date === "date-3") {
+      $("#dwight").show();
+      else if (quality === "quality-1" && date === "date-3") {
+       $("#") 
       }
+    } else {
+      $("#michael").show();
     }
-
     event.preventDefault();
   });
 });
+
+//   if (gender === "male") {
+//     if (quality === "quality-1" && date === "date-1") {
+//       $("#kevin").show();
+//     }
